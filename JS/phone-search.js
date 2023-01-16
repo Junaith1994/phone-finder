ad5ef63cda483c445c81f66602f647ad5da095fa
Junaith1994 <daya2018ctg@gmail.com>
2023-01-17 01:08:59 +0600

    Added Detail Info about phone when clicked, Added 'No Result Found' message

--- JS/phone-search.js
+++ JS/phone-search.js
@@ -7,6 +7,7 @@ const loadData = () => {
     
     // Clearing input field
     document.getElementById('search-field').value = '';
+    // document.getElementById('detail-info').textContent = '';
 }
 
 const displaySearchResult = results => {
@@ -16,7 +17,9 @@ const displaySearchResult = results => {
     containerDiv.textContent = '';
 
     if(results.length === 0) {
+        console.log(results.length);
         document.getElementById('no-result').setAttribute('class', 'd-block');
+        // document.getElementById('detail-info').textContent = '';
     }
     else {
         document.getElementById('no-result').setAttribute('class', 'd-none');
@@ -31,7 +34,7 @@ const displaySearchResult = results => {
                     <img src="${result.image}" class="w-25 card-img-top" alt="phone-image">
                     <h5 class="card-title">Model: ${result.phone_name}</h5>
                     <h5 class="card-text">Brand: ${result.brand}</h5>
-                    <button onclick="loadDetailInfo('${result.slug}')" class="btn btn-primary" type="submit">Details</button>
+                    <button onclick="loadDetailInfo('${result.slug}')" class="btn btn-primary fw-semibold" type="submit">Details</button>
                 </div>
             </div>
         `;
@@ -48,5 +51,30 @@ const loadDetailInfo = phoneId => {
 
 const displayDetailInfo = details => {
     const detailInfoDiv = document.getElementById('detail-info');
-    console.log(details);
+    // console.log(details);
+    detailInfoDiv.classList.add('card', 'mb-3')
+    
+    detailInfoDiv.innerHTML = `
+        <div class="card-body">
+            <img src="${details.image}" class="w-25 card-img-top mx-auto d-block" alt="phone-image">
+            <h5 class="card-title">${details.releaseDate? details.releaseDate: 'No release date found'}</h5>
+            <p id="main-features" class="card-text">
+                <p><b>Chip Set:</b> ${details.mainFeatures.chipSet}</p>
+                <p><b>Display Size:</b> ${details.mainFeatures.displaySize}</p>
+                <p><b>Memory:</b> ${details.mainFeatures.memory}</p>
+                <p><b>Storage:</b> ${details.mainFeatures.storage}</p> 
+            </p>
+        </div>
+    `;
+    const p = document.createElement('p');
+    document.getElementById('main-features').appendChild(p);
+    const sensors = details.mainFeatures.sensors;
+    p.innerHTML = `<b>Sensors:</b>`;
+    for (const sensor of sensors) {
+        const sensonrsInfo = document.createElement('span');
+        sensonrsInfo.classList.add('px-2');
+        sensonrsInfo.innerText = sensor;
+        p.appendChild(sensonrsInfo)
+        // console.log(sensor);
+    }
 }
